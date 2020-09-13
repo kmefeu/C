728084544fb7b714636f9909c51d0c800eb90707
kmefeu <67432473+kmefeu@users.noreply.github.com>
2020-09-13 16:54:23 -0300

    Ex10

--- S2-CLASS-4-STRUCT/ex10-cadastro-ordem-alfabetica.c
+++ S2-CLASS-4-STRUCT/ex10-cadastro-ordem-alfabetica.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#define n 3
+#define n 5
 
 struct info
 {
@@ -23,28 +23,32 @@ int main()
 {
     system("cls");
 
-    char alf[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'};
+    char alf[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'}; // String alfabeto para comparar com o nome // Alphabetic string to compare with the name
 
-    int i, ii, iii, order[n];
+    int i, ii, iii, alfaOrder[n];
 
-    for (i = 0; i < n; i++) // Grava os dados
+    for (i = 0; i < n; i++) // Grava os dados // Input of data
     {
 
         printf("\n\nInsert: Name Address Telephone\n");
         scanf("%s%s%s", &users[i].name, &users[i].address, &users[i].telephone);
+
+        alfaOrder[i] = i; // Para que o vetor em ordem alfabética fique igual a original facilitando as operações de troca de posição // Creat a copy from the original vector making easy to change places latter
     }
 
-    for (i = 0; i < n; i++) // Para cada Usuário
+    for (i = 0; i < n; i++) // Para cada Usuário // For each user
     {
-        for (ii = 0; ii < strlen(users[i].name); ii++) // Para cada letra
+        for (ii = 0; ii < strlen(users[i].name); ii++) // Para cada letra // For each letter
         {
             users[i].name[ii] = tolower(users[i].name[ii]);
 
-            for (iii = 0; iii < strlen(alf); iii++) // Compara com cada letra do alfabeto e gera um nome id string
+            for (iii = 0; iii < strlen(alf); iii++) // Compara cada letra do nome com cada letra do alfabeto e gera uma string com numeros equivalente a letras // Compare each letter of the name with each letter on the alphabet and them generate a string with id numbers
             {
 
                 if (users[i].name[ii] == alf[iii])
                 {
+
+                    // Tire o comentário caso queira ver o algoritimo // Remove the coment to check the algorithm
                     // printf("%c", users[i].name[ii]);
                     // printf("%c", alf[iii]);
                     // printf("%d", iii);
@@ -56,33 +60,35 @@ int main()
         }
     }
 
-    for (i = 0; i < n; i++) // Para cada usuário
+    for (i = 0; i < n; i++) // Para cada usuário // For each user
     {
 
-        for (ii = i + 1; ii < n; ii++) // compara usuário com usuário
+        for (ii = i + 1; ii < n; ii++) // Compara usuário com usuário // Compare to another user
         {
-            int smallest = (strlen(users[i].name) <= strlen(users[ii].name)) ? users[i].name : users[ii].name; // pega o comprimento do menor nome
+            int shortest = (strlen(users[alfaOrder[i]].name) < strlen(users[alfaOrder[ii]].name)) ? users[alfaOrder[i]].name : users[alfaOrder[ii]].name; // Pega o comprimento do menor nome para evitar comparação vazia// Use the shortest name length to avoide comparing null values
 
-            for (iii = 0; iii < smallest; iii++) // Compara número a número do nome id
+            for (iii = 0; iii < shortest; iii++) // Compara número a número de cada nomeId // For each number/letter in the name/nameId
             {
-                if (users[i].nameId[iii] < users[ii].nameId[iii]) // Quando o numero atual é menor que o anterio ele sobe o nome 1 posição
+
+                // Tire o comentário caso queira ver o algoritimo // Remove the coment to check the algorithm
+
+                // if (users[alfaOrder[i]].nameId[iii] < users[alfaOrder[ii]].nameId[iii]) // Quando o numero do nome i é menor que o ii não tem troca de lugar // When the number in name i is smaller than the number in the name ii nothing happens
+                // {
+                //     printf("\n\n %s[%d][%d] = %c < %s[%d][%d] = %c", users[alfaOrder[i]].name, i, iii, users[alfaOrder[i]].name[iii], users[alfaOrder[ii]].name, ii, iii, users[alfaOrder[ii]].name[iii]);
+                //     break;
+                // }
+
+                if (users[alfaOrder[i]].nameId[iii] > users[alfaOrder[ii]].nameId[iii]) // Quando o numero do nome i é maior que o numero de ii eles trocam de lugar // When i is bigger than ii they switch places
                 {
-                    printf("\n\nuser[%d].name[%d] = %c < nuser[%d].name[%d] = %c", i, iii, users[i].name[iii], ii, iii, users[ii].name[iii]);
+                    // printf("\n\n %s[%d][%d] = %c > %s[%d][%d] = %c", users[alfaOrder[i]].name, i, iii, users[alfaOrder[i]].name[iii], users[alfaOrder[ii]].name, ii, iii, users[alfaOrder[ii]].name[iii]);
 
-                    order[i] = i;
+                    // printf("\nAlfaOrder[%d ] = %s", i, users[alfaOrder[ii]].name);
+                    // printf("\nAlfaOrder[%d + %d] = %s \n\n", ii, users[alfaOrder[i]].name);
 
-                    printf("\norder[i] = %d \n\n", i);
-                    
-                    break;
-                }
+                    int placeholder = alfaOrder[i]; // guarda provisoriamente o conteudo da posição i // holds the content of i for a moment
 
-                if (users[i].nameId[iii] > users[ii].nameId[iii])
-                {
-                    printf("\n\nuser[%d].name[%d] = %c > nuser[%d].name[%d] = %c", i, iii, users[i].name[iii], ii, iii, users[ii].name[iii]);
-
-                    order[i] = ii;
-
-                    printf("\norder[ii] = %d \n\n", ii);
+                    alfaOrder[i] = alfaOrder[ii];
+                    alfaOrder[ii] = placeholder;
 
                     break;
                 }
@@ -94,24 +100,26 @@ int main()
 
     for (i = 0; i < n; i++)
     {
-        printf("\n\nName:%s\nAddress:%s\nTelephone:%s\n\n", users[order[i]].name, users[order[i]].address, users[order[i]].telephone);
+        printf("\n\nName:%s\nAddress:%s\nTelephone:%s\n\n", users[alfaOrder[i]].name, users[alfaOrder[i]].address, users[alfaOrder[i]].telephone);
     }
 
-    printf("\nInsert Order\n");
+    // Tire o comentário caso queira confirmar a ordem de entrada // Remove the coment to check the insert order
 
-    for (i = 0; i < n; i++)
-    {
-        printf("\n\nName:%s\nAddress:%s\nTelephone:%s\n", users[i].name, users[i].address, users[i].telephone);
-        printf("NameId:");
+    // printf("\nInsert Order\n"); // Ordem de entrada
 
-        for (ii = 0; ii < 10; ii++)
-        {
+    // for (i = 0; i < n; i++)
+    // {
+    //     printf("\n\nName:%s\nAddress:%s\nTelephone:%s\n", users[i].name, users[i].address, users[i].telephone);
+    //     printf("NameId:");
 
-            printf("%i ", users[i].nameId[ii]);
-        }
+    //     for (ii = 0; ii < 10; ii++) // Para confirmar os valores do nomeId // To confirm the values of nameId
+    //     {
 
-        printf("\n\n");
-    }
+    //         printf("%i ", users[i].nameId[ii]);
+    //     }
+
+    //     printf("\n\n");
+    // }
 
     printf("\n\n");
 
